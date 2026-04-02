347b07129936bb4a9b85faa05839b0ad8cd6cc8a
Ishantgarg-web <ishantkumar1412@gmail.com>
2026-04-02 16:16:14 +0530

    AdaptorDesignPattern - Base Ready

---
+++ src/adaptorDsignPattern/Main.java
@@ -0,0 +1,16 @@
+package adaptorDsignPattern;
+
+/**
+ * Adaptor Design Pattern: It just behaves like a adaptor converting AC into DC current or vice-versa.
+ * Example: You already have some code in your running application that is using code from Vendor A, But somehow contract with Vendor A is
+ * closed, and Vendor B opens the new contract. and their defined classes are little bit different. How will you incorporate VendorB
+ * in your application?
+ * 1. Change your code to compatible with Vendor B, but it will not a good solution.
+ * 2. Create a Adaptor between your code and Vendor B, So that it will fits the both requirements.
+ *
+ * Nomenclature:
+ * 1. target interface: Adaptor
+ * 2. Those we want to change into target: Adaptee
+ */
+public class Main {
+}
